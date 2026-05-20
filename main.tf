9fe6d87f2e4048890b9a25f96774695c594b3778
Roshan <45045902+rchandnaWUSTL@users.noreply.github.com>
2026-05-20 15:02:12 -0400

    Matrix upgrade upgrade-test-0.8.8 -> 1.15.3 (#2)
    * matrix: pin required_version to 0.8.8 on upgrade-test-0.8.8
    
    * matrix: upgrade required_version to 1.15.3 on upgrade-test-0.8.8

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 terraform {
   required_version = "1.15.3"
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
